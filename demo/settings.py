48680f19700ffa84ac0c24cde9a7fdee73feb632
Philip sam <Philipsamaras@gmail.com>
2024-03-12 14:50:33 +0000

    Add allowed hosts

--- demo/settings.py
+++ demo/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://my-project-4-524b015374ef.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['my-project-4-524b015374ef.herokuapp.com/', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://8000-philippos123-projket4-y1lgtm9j62d.ws-eu108.gitpod.io','https://8000-philippos123-projket4-y1lgtm9j62d.ws-eu108.gitpod.io']
 
 
