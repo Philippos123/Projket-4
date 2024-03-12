eedd680c41070ef1bf888b31ba50294ad4e097b2
Philip sam <Philipsamaras@gmail.com>
2024-03-12 21:16:22 +0000

    Fxining so the images are visible

--- demo/settings.py
+++ demo/settings.py
@@ -168,3 +168,6 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
